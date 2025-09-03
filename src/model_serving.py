9275268acaf6621a9ea3b6e303385ea9a18dc492
StrokeXXX <cammitri@gmail.com>
2025-09-03 17:45:31 +0200

    Update model_serving.py

--- src/model_serving.py
+++ src/model_serving.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+import mlflow.pyfunc
 
+app = FastAPI()
+model = mlflow.pyfunc.load_model("models:/housing-model/production")
+
+@app.post("/predict")
+async def predict(features: dict):
+    prediction = model.predict([list(features.values())])
+    return {"prediction": float(prediction[0])}
